db28483383f1a57e843e8f427ff15f37848a666a
Prita <prita.ghosh@bt.com>
2020-10-11 17:18:49 +0100

    v1.0.4

--- btplayersetup.rb
+++ btplayersetup.rb
@@ -4,8 +4,8 @@
 class Btplayersetup < Formula
   desc ""
   homepage ""
-  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.3.tar.gz"
-  sha256 "95e0b17cc132ca42ab81c9373162785d2c7ae9462fed3409d56d628b5ede375e"
+  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.4.tar.gz"
+  sha256 "3baf8345b4f3f0d9cc876fa7b0d92bffa827618fc664290e3ed9427e48fd01ee"
   license ""
 
   def install
