16457d9f566b76e82c3bbbf304dde5a7960b3c25
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-11 14:30:15 +0100

    updated formula

--- btplayersetup.rb
+++ btplayersetup.rb
@@ -4,12 +4,11 @@
 class Btplayersetup < Formula
   desc ""
   homepage ""
-  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.0.tar.gz"
-  sha256 "fa4fb60fa82ac63b95cef20ca646e49b9de0dd6eb7dfb049c95b276a88978a53"
+  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.1.tar.gz"
+  sha256 "affe623359fbb6b20b7fd8c9596e284dfe9536865ba1a33d516b863c8dc1930c"
   license ""
 
   def install
     bin.install "btplayersetup"
   end
-
 end
