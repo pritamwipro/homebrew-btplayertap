409963fd973924f7618f7c306b2fac01dd6a1f36
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-11 11:20:00 +0100

    add formula

---
+++ btplayersetup.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Btplayersetup < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.0.tar.gz"
+  sha256 "fa4fb60fa82ac63b95cef20ca646e49b9de0dd6eb7dfb049c95b276a88978a53"
+  license ""
+
+  def install
+    bin.install "btplayersetup"
+  end
+
+end
