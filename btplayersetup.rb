73d210fb7864745acd3ede016be8ffeea47c20e0
Pritam <pritam.ghosh@bt.com>
2020-10-11 18:52:21 +0100

    minor changes

--- btplayersetup.rb
+++ btplayersetup.rb
@@ -4,9 +4,11 @@
 class Btplayersetup < Formula
   desc ""
   homepage ""
-  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.5.tar.gz"
-  sha256 "5c0fbc46b4882df3207086b16465864a9c4519c457f2b74f667fd51525f54697"
+  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.6.tar.gz"
+  sha256 "1c7dae578d731ea2e6774e63c316b60fed53c29430cfb60928cd2c979750005b"
   license ""
+
+  # depends_on "cmake" => :build
 
   def install
     bin.install "btplayersetup"
