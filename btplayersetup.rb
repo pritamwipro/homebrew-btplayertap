a63a2ffbee40993972874e799fb6ef1bc289c215
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-11 20:12:27 +0100

    fix folder name issue

--- btplayersetup.rb
+++ btplayersetup.rb
@@ -4,11 +4,9 @@
 class Btplayersetup < Formula
   desc ""
   homepage ""
-  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.6.tar.gz"
-  sha256 "1c7dae578d731ea2e6774e63c316b60fed53c29430cfb60928cd2c979750005b"
+  url "https://github.com/pritamwipro/btplayersetup/archive/v1.0.7.tar.gz"
+  sha256 "30f012c5a1e5c51002fb2def9aac1e0738d03e0c0c3a0e262bd37266ed171b4f"
   license ""
-
-  # depends_on "cmake" => :build
 
   def install
     bin.install "btplayersetup"
