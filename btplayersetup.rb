6a8727e28bf9ae9ca4a5dca8fe0035845ac4b956
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-13 08:31:26 +0100

    fix

--- btplayersetup.rb
+++ btplayersetup.rb
@@ -4,12 +4,11 @@
 class Btplayersetup < Formula
   desc ""
   homepage ""
-  url "https://github.com/pritamwipro/btplayersetup/archive/v1.1.0.tar.gz"
-  sha256 "3db082bba2b34ae7d3311dc9fff581cfa72daca6284eaf4ad503bdefeeb1f3c0"
+  url "https://github.com/pritamwipro/btplayersetup/archive/v1.1.1.tar.gz"
+  sha256 "89432b622315514a89d29e0d0dc362299b31b2e3db71ff878b5766c8aeb709e0"
   license ""
 
-  depends_on "firefox"
-  depends_on "google-chrome"
+  depends_on cask: 'virtualbox'
 
   def install
     bin.install "btplayersetup"
