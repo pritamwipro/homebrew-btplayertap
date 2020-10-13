6d764056ad0239ec37948474f57d79fe380ef2d5
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-13 09:44:46 +0100

    test

--- btplayersetup.rb
+++ btplayersetup.rb
@@ -4,10 +4,9 @@
 class Btplayersetup < Formula
   desc ""
   homepage ""
-  url "https://github.com/pritamwipro/btplayersetup/archive/v1.1.3.tar.gz"
-  sha256 "05c8e095bfb41913ecd4c57843b2581956a6141fc40b19a3530b52fe6225c47c"
+  url "https://github.com/pritamwipro/btplayersetup/archive/v1.1.5.tar.gz"
+  sha256 "728a57c3d4b14a5cc03b6bad90e2412c33c325d54eb808a8eb871b8c96036429"
   license ""
-
   # depends_on "cmake" => :build
 
   depends_on "tree" => :build
